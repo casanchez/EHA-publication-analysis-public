5a96226054c579538dba2e49727f53f335554b51
casanchez <sanchez@ecohealthalliance.org>
2026-05-11 20:36:23 -0400

    tweaking code looking at geography alignment

--- R/compare_geography.R
+++ R/compare_geography.R
@@ -7,70 +7,68 @@
 #' @author Cecilia Sanchez
 #' @export
 compare_geography <- function(publication_data, authorship_data){
-
-# restrict to only papers that were about a given country/countries
-pubs_with_focal_country <- publication_data %>% 
-  #mutate(country_of_study = gsub("\"", "", country_of_study)) %>% 
-  filter(country_of_study != "Non-specific")
-
-# set up empty columns
-# pubs_with_focal_country$first_auth_cc <- NA
-# pubs_with_focal_country$last_auth_cc <- NA
-pubs_with_focal_country$first_geo_match <- NA
-pubs_with_focal_country$last_geo_match <- NA
-pubs_with_focal_country$either_geo_match <- NA
-
-# for a given publication:
-for(i in pubs_with_focal_country$identifier){
-
-  # get the country code of first author
-  first_auth_cc <- authorship_data %>% 
-    filter(publication_link == i,
-           authorship_position == "first") %>% 
-    select(iso2) %>% 
-    distinct() %>% 
-    pull()
   
-  # get the country code of last author
-  last_auth_cc <- authorship_data %>% 
-    filter(publication_link == i,
-           authorship_position == "last") %>% 
-    select(iso2) %>% 
-    distinct() %>% 
-    pull()
+  # restrict to only papers that were about a given country/countries
+  pubs_with_focal_country <- publication_data %>% 
+    #mutate(country_of_study = gsub("\"", "", country_of_study)) %>% 
+    filter(country_of_study != "Non-specific")
   
-  # get the country codes of the focal country/ies
-  paper_cc <- pubs_with_focal_country %>% 
-    filter(identifier == i) %>% 
-    pull(cc_of_study)
+  # set up empty columns
+  pubs_with_focal_country$first_auth_cc <- NA
+  pubs_with_focal_country$last_auth_cc <- NA
   
-  # test if first author cc matches with any of the focal countries 
-  first_match <- str_detect(string = paper_cc, pattern = first_auth_cc)
-  # store the result
-  ## first author may be filtered from results...
-  if(rlang::is_empty(first_auth_cc)){
-    first_match <- NA
+  # for a given publication:
+  for(i in pubs_with_focal_country$identifier){
+    
+    # get the country code of first author
+    first_auth_cc <- authorship_data %>% 
+      filter(publication_link == i,
+             authorship_position == "first") %>% 
+      select(iso2) %>% 
+      distinct() %>% 
+      pull()
+    
+    # save country code of first author
+    pubs_with_focal_country$first_auth_cc[pubs_with_focal_country$identifier == i] <- first_auth_cc
+    
+    # get the country code of last author
+    last_auth_cc <- authorship_data %>% 
+      filter(publication_link == i,
+             authorship_position == "last") %>% 
+      select(iso2) %>% 
+      distinct() %>% 
+      pull()
+    
+    # save country code of last author (if there was a last author)
+    if(!is_empty(last_auth_cc)){
+      pubs_with_focal_country$last_auth_cc[pubs_with_focal_country$identifier == i] <- last_auth_cc
+    }
+    
   }
   
-  pubs_with_focal_country$first_geo_match[pubs_with_focal_country$identifier == i] <- first_match
+  geo_matches <- pubs_with_focal_country %>% 
+    mutate(
+      first_geo_match = case_when(
+        str_detect(string = cc_of_study, pattern = first_auth_cc) ~ "yes",
+        is.na(first_auth_cc) ~ NA,
+        .default = "no"),
+      last_geo_match = case_when(
+        str_detect(string = cc_of_study, pattern = last_auth_cc) ~ "yes",
+        is.na(last_auth_cc) ~ NA,
+        .default = "no"),
+      either_geo_match = case_when(
+        first_geo_match == "yes" | last_geo_match == "yes" ~ "yes",
+        .default = "no"),
+      both_geo_match = case_when(
+        first_geo_match == "yes" & last_geo_match == "yes" ~ "yes",
+        # can only have both authors match when the paper had first and last authors
+        is.na(last_geo_match) ~ NA,
+        .default = "no")
+    )
   
-  # was there a last author?
-  if(is_empty(last_auth_cc)){
-    # if no, then we can't evaluate a match
-    last_match <- NA
-  } else{
-    # if there is, test if last author cc matches with any of the focal countries 
-    last_match <- str_detect(string = paper_cc, pattern = last_auth_cc)
-    }
-  # store the result
-  pubs_with_focal_country$last_geo_match[pubs_with_focal_country$identifier == i] <- last_match
-
-  # note if either author matched
-  pubs_with_focal_country$either_geo_match[pubs_with_focal_country$identifier == i] <- sum(first_match, last_match, na.rm = T)
-}
-
-publication_data_with_geo <- left_join(publication_data, pubs_with_focal_country)
-
-return(publication_data_with_geo)
-
+  publication_data_with_geo <- left_join(publication_data, geo_matches) %>% 
+    mutate(cc_of_study = na_if(cc_of_study, "NA"))
+  
+  return(publication_data_with_geo)
+  
 }
